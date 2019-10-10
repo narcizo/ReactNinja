46ca19e297d5ad777d1f939c87cbf75b8e883344
Narcizo Gabriel <narcizo.gabriel2@gmail.com>
2019-10-10 18:09:14 -0300

    Boilerplate concluido

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+    render: function(){
+        return <h1>Aplicação</h1>
+    }
+})
+
+// padrão ecma script 2015
+export default App
