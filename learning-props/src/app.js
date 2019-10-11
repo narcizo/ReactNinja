e18a7d66cfbac247a842d9e1df1b6cf243bc9285
Narcizo <narcizo.gabriel2@gmail.com>
2019-10-11 15:26:30 -0300

    Criando componentes com Classes

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -1,20 +1,29 @@
 'use strict'
 
-import React from 'react'
-import title from './title'
+import React, { Component } from 'react'
 import Title from './title'
 
-const App = React.createClass({
-    render: function(){
-        return (
-        <div className='container'>
-            <Title name = 'narcizo' lastname='garbiel' />
-            <label htmlFor='input'>Input</label>
-            <input type='text' id='input'/>
-        </div>
+class App extends Component{
+    render (){
+        return(
+            <div className='container'>
+                <Title name = 'narcizo' lastname='gabriel' />
+            </div>
         )
     }
-})
+}
+
+// const App = React.createClass({
+//     render: function(){
+//         return (
+//         <div className='container'>
+//             <Title name = 'narcizo' lastname='garbiel' />
+//             <label htmlFor='input'>Input</label>
+//             <input type='text' id='input'/>
+//         </div>
+//         )
+//     }
+// })
 
 // padrão ecma script 2015
 export default App
