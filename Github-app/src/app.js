88cdc38b6c3fc41d952cffe33de60699080b69c2
Narcizo <narcizo.gabriel2@gmail.com>
2019-11-07 14:35:51 -0300

    alguma coisa de componentes

--- Github-app/src/app.js
+++ Github-app/src/app.js
@@ -1,40 +1,38 @@
 'use strict'
 
-import React from 'react'
-import Search from './components/search'
-import UserInfo from './components/user-info'
-import Actions from './components/actions'
-import Repos from './components/repos'
+import React, { Component } from 'react'
+import AppContent from './components/app-content'
 
 
-const App = React.createClass({
-  render: function () {
-    return (
-      <div className='app'>
-        <Search />
-        <UserInfo />
-        <Actions />
-
-        <Repos
-          className='repos'
-          title='Repositórios:'
-          repos={[{
-            name: "Nome do repositório",
-            link: '#'
-          }]} />
-
-        <Repos
-          className='starred'
-          title='Favoritos:'
-          repos={[{
-            name: "Nome do repositório",
-            link: '#'
-          }]} />
-
-      </div>
-    )
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      userinfo: {
+        username: 'Narcizo Gabriel',
+        repos: 12,
+        followers: 10,
+        following: 8
+      },
+      repos: [{
+        name: 'Repo',
+        link: '#'
+      }],
+      starred: [{
+        name: 'Repo',
+        link: '#'
+      }]
+    }
   }
-})
+
+  render() {
+    return <AppContent 
+      userinfo={this.state.userinfo}
+      repos = {this.state.repos}
+      starred = {this.state.starred}
+    />
+  }
+}
 
 // padrão ecma script 2015
 export default App
